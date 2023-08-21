89bd39fc4f84e54e7ac64cfd210e5f99b3607dd0
Nainil-APNT <nainil.chaudhari@apnttechnolabs@gmail.com>
2023-08-21 19:35:47 +0530

    clear error while using href so clear it

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -10,7 +10,6 @@ const Employee = ({employee,deleteEmployee}) => {
       navigate(`/editEmployee/${id}`);
     }
 
-
   return (
     <tr key={employee.id}>
     <td className='text-left px-6 py-4 whitespace-nowrap'><div className='text-sm text-gray-500 '>{employee.name}</div></td>
@@ -19,9 +18,26 @@ const Employee = ({employee,deleteEmployee}) => {
     <td className='text-left px-6 py-4 whitespace-nowrap'><div className='text-sm text-gray-500 '>{employee.username}</div></td>
     <td className='text-left px-6 py-4 whitespace-nowrap'><div className='text-sm text-gray-500 '>{employee.password}</div></td>
     <td className='text-right px-6 py-4 whitespace-nowrap font-medium text-sm'>
-      <a onClick={(e,id)=> editEmployee(e,employee.id)} className='text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer'>Edit </a>
+   
+     {/*  <a onClick={(e,id)=> editEmployee(e,employee.id)} className='text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer'>Edit </a>
       <a onClick={(e,id)=>deleteEmployee(e,employee.id)} className='text-red-500 hover:text-red-800  hover:cursor-pointer'>Delete </a>
-    
+     */}
+
+<button
+  onClick={(e) => editEmployee(e, employee.id)}
+  className="text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer"
+>
+  Edit
+</button>
+<button
+  onClick={(e) => deleteEmployee(e, employee.id)}
+  className="text-red-500 hover:text-red-800 hover:cursor-pointer"
+>
+  Delete
+</button>
+
+
+
     </td>
 
 </tr> 
